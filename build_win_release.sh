6c8962673521a98ccb2b1e8caa34a51303fbf80a
Linus_Probert <linus.probert@netset.se>
2016-04-15 13:35:16 +0200

    Add zip step to build

--- build_win_release.sh
+++ build_win_release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Building"
 mkdir -p build/release
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain-archlinux-x86_64-mingw32.cmake ..
@@ -7,6 +8,7 @@ make
 cp lowrezjam.exe release/.
 cd ..
 
+echo "Copying files"
 cp /usr/x86_64-w64-mingw32/bin/SDL2.dll build/release/.
 cp /usr/x86_64-w64-mingw32/bin/SDL2_image.dll build/release/.
 cp /usr/x86_64-w64-mingw32/bin/SDL2_mixer.dll build/release/.
@@ -21,3 +23,10 @@ cp /usr/x86_64-w64-mingw32/bin/zlib1.dll build/release/.
 
 cp -r sounds build/release/.
 cp -r textures build/release/.
+
+mv build/release build/LowRezJam
+
+echo "Compressing"
+cd build
+zip -r LowRezJam.zip LowRezJam/
+cd ..
